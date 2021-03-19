a606b183c24e7346334684fffbf4f42ebab855e3
Jacob Anthony Tabares <73261191+JetPlaneJay@users.noreply.github.com>
2021-03-18 21:23:52 -0700

    Create Instructors.vue

---
+++ components/Instructors.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+        <h2 class='section-heading'>Instructors</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>First Name</th>
+                    <th>Middle Initial</th>
+                    <th>Last Name</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="instructor in instructors" v-bind:key="instructor">
+                    <td>{{ instructor.firstName }}</td>
+                    <td>{{instructor.middleInitial}}
+                    <td>{{ instructor.lastName }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
+
+
+<script>
+    export default {
+        name: 'Instructors',
+        mounted() {
+            this.instructors = getInstructors();
+        },
+        data () {
+            return {
+                instructors: []
+            }
+        }
+    }
+    function getInstructors() {
+        return JSON.parse(instructors);
+    }
+    var instructors = '[{"firstName": "Gerald", "middleInitial": "H", "lastName": "Adler"}, {"firstName": "Michael", "middleInitial": "M", "lastName": "Margarito"}, {"firstName": "Xi", "middleInitial": "V", "lastName": "Chu"}]';
+</script>
+
+<style scoped>
+    table, th, td {
+        padding: 50px;
+        border: 25px solid purple; 
+        border-collapse: collapse;
+        margin-left: auto;
+        margin-right: auto;
+    }
+</style>
