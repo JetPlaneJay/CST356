20fcc353510f6565dffcb259fe45cbc79f484fb5
Jacob Anthony Tabares <73261191+JetPlaneJay@users.noreply.github.com>
2021-03-18 22:12:16 -0700

    Update index.js

--- webapp/router/index.js
+++ webapp/router/index.js
@@ -1,14 +1,44 @@
 import Vue from 'vue';
-import axios from 'axios';
-import VueAxios from 'vue-axios';
-import App from './App';
-import router from './router';
+import Router from 'vue-router';
+import Home from '@/components/Home';
+import Instructors from '@/components/Instructors';
+import Students from '@/components/Students';
 
-Vue.config.productionTip = false;
-Vue.use(VueAxios, axios);
+Vue.use(Router)
 
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      component: Home
+    },
+    {
+      path: '/home',
+      component: Home
+    },
+    {
+      path: '/Instructors',
+      component: Instructors
+    },
+    {
+      path: '/Students',
+      component: Students
+    }
+  ]
+})
 
-new Vue({
-  router,
-  render: h => h(App),
-}).$mount('#app');
+import { authGuard } from "../auth/authGuard";
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    // .. other routes ..
+    {
+      path: "/students",
+      name: "students",
+      component: Students,
+      beforeEnter: authGuard
+    }
+  ]
+});
