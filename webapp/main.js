4ee668fe968f0969a8c15abcd9ca5fe4ad8b494c
Jacob Anthony Tabares <73261191+JetPlaneJay@users.noreply.github.com>
2021-03-18 22:12:39 -0700

    Update main.js

--- webapp/main.js
+++ webapp/main.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import VueAxios from 'vue-axios';
 import App from './App';
 import router from './router';
+import { domain, clientId } from "../auth_config.json";
+import { Auth0Plugin } from "./auth";
 
 Vue.config.productionTip = false;
 Vue.use(VueAxios, axios);
@@ -12,3 +14,16 @@ new Vue({
   router,
   render: h => h(App),
 }).$mount('#app');
+
+
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: appState => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  }
+});
